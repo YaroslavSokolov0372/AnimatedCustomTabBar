81a621b02529be657c26021b01e3372662f97ef8
YaroslavSokolov0372 <yaroslav.sokolov0372@gmail.com>
2023-07-06 15:42:56 +0200

    Initial Commit

---
+++ AnimatedCustomTabBar/AnimatedCustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedCustomTabBarApp.swift
+//  AnimatedCustomTabBar
+//
+//  Created by Yaroslav Sokolov on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedCustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedCustomTabBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AnimatedCustomTabBar
+//
+//  Created by Yaroslav Sokolov on 06/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
